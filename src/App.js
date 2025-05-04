4bbc7b82583491a237c26a51f2adf83fe5658e84
Hansraj <hraj2662001@gmail.com>
2025-05-04 21:42:47 +0530

    add poster

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";import DiscoverEvents from "./DiscoverEvents";
 import "./App.css";
+import "../public/images/421222A0-8D08-4641-B0EF-E46EC6C2A745_1_201_a.jpeg"
 
 function App() {
   const [formData, setFormData] = useState({
@@ -58,7 +59,7 @@ function App() {
               {/* Left: Poster & Details */}
               <div className="event-left">
                 <img
-                  src="/images/421222A0-8D08-4641-B0EF-E46EC6C2A745_1_201_a.jpeg"
+                  src=""
                   alt="AI Workshop Series Poster"
                   className="event-poster"
                 />
